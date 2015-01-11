d335e7a39c68260bfe1a8091608f3e0fd440dadf
Roy de Jong <roy@softwarepunt.nl>
2015-01-11 21:38:15 +0100

    Score/UI: Add veggie counter (fail condition). Add game over when reaching your five a day. Add constant score increments as you play.

--- src/game/score.js
+++ src/game/score.js
@@ -2,14 +2,34 @@ var Score = {
     current: 0,
     donuts: 0,
     veggie: 0,
+    isGameOver: false,
 
     init: function() {
-        window.setInterval(this.updateUi.bind(this), 250);
+        var updateLoop = function() {
+            if (!this.isGameOver) {
+                this.addScore(10);
+            }
+            this.updateUi();
+        }.bind(this);
+
+        window.setInterval(updateLoop, 250);
         this.updateUi();
     },
 
     updateUi: function() {
         $('#score').text(this.getString());
+
+        var $fiveADay = $('#fiveaday');
+
+        for (var i = 1; i <= 5; i++) {
+            var $step = $fiveADay.find('.step.s' + i);
+
+            if (this.veggie >= i) {
+                $step.addClass('active');
+            } else {
+                $step.removeClass('active');
+            }
+        }
     },
 
     getString: function() {
@@ -28,6 +48,10 @@ var Score = {
 
     addScore: function(amt) {
         this.current += amt;
+
+        if (this.current < 0) {
+            this.current = 0;
+        }
     },
 
     addDonut: function() {
@@ -36,8 +60,30 @@ var Score = {
     },
 
     addVeggie: function() {
+        if (this.veggie >= 5) {
+            return;
+        }
+
         this.veggie++;
         this.addScore(-250);
+
+        if (this.veggie >= 5) {
+            this.gameOver("Watch out for your five a day! It keeps the doctor away!<br />(That's not what we want)");
+        }
+    },
+
+    gameOver: function(reason) {
+        if (this.isGameOver) {
+            return;
+        }
+
+        this.isGameOver = true;
+
+        Map.velocity = 0;
+
+        var $gameOver = $('#gameover');
+        $gameOver.find('h2').html(reason);
+        $gameOver.slideDown();
     },
 
     reset: function() {
