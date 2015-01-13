0c97c782e326240fd2a1fff139a243701a3b5291
Roy de Jong <roy@softwarepunt.nl>
2015-01-13 23:23:22 +0100

    Game: Add UI for speed rate. Increment speed slowly every few seconds, not constantly. Perform Score updates on the game loop, not on window timers.

--- src/game/score.js
+++ src/game/score.js
@@ -3,17 +3,10 @@ var Score = {
     donuts: 0,
     veggie: 0,
     isGameOver: false,
+    updateLoopTime: 0,
+    speedIncrementTime: 0,
 
     init: function() {
-        var updateLoop = function() {
-            if (!this.isGameOver) {
-                this.addScore(10);
-                Map.velocity += 0.01;
-            }
-            this.updateUi();
-        }.bind(this);
-
-        window.setInterval(updateLoop, 250);
         this.updateUi();
     },
 
@@ -30,6 +23,14 @@ var Score = {
             } else {
                 $step.removeClass('active');
             }
+        }
+
+        $('#speed').text('Speed x' + (Map.velocity - Map.STARTING_VELOCITY + 1));
+
+        if (Map.velocity <= 0) {
+            $('#speed').hide();
+        } else {
+            $('#speed').show();
         }
     },
 
@@ -78,6 +79,28 @@ var Score = {
         if (this.isGameOver && Keyboard.wasKeyPressed(KeyEvent.DOM_VK_SPACE)) {
             Map.newGame();
         }
+
+        if (this.updateLoopTime > 0) {
+            this.updateLoopTime--;
+        }
+
+        if (this.updateLoopTime <= 0) {
+            if (!this.isGameOver) {
+                this.addScore(10);
+            }
+
+            this.updateUi();
+            this.updateLoopTime = 15; // 250ms, or something like that
+        }
+
+        if (this.speedIncrementTime > 0) {
+            this.speedIncrementTime--;
+        }
+
+        if (this.speedIncrementTime <= 0 && !this.isGameOver) {
+            Map.velocity += 0.5;
+            this.speedIncrementTime = (60 * 10); // Every ten seconds or so
+        }
     },
 
     gameOver: function(reason) {
@@ -101,5 +124,7 @@ var Score = {
         this.current = 0;
         this.donuts = 0;
         this.veggie = 0;
+        this.updateLoopTime = 0;
+        this.speedIncrementTime = 0;
     }
 };
